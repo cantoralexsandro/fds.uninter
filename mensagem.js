489141a600f31fa0d07e5ae6db38cf7119875ee9
cantoralexsandro <ocantoralexsandro@gmail.com>
2025-06-17 16:06:11 -0300

    Add files via upload

---
+++ mensagem.js
@@ -0,0 +1,10 @@
+let mensagem = document.querySelector("#Botão")
+
+mensagem.addEventListener("click",trocaTexto);
+
+
+
+function trocaTexto(){
+        mensagem.innerHTML="O que fizermos hoje, ecoará na eternidade!";
+
+}
